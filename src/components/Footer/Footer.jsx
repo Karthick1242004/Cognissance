a4e6e1d2ac0232179a94b77deb69c1c54f292f00
karthick <93571352+Karthick1242004@users.noreply.github.com>
2024-03-23 15:26:42 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
         <p>Contact : Srinath V V (+91 9042967878)</p>
         <p>Contact : Gokulsundar S (+91 7010740253)</p>
          <p><i>© CSEA 2024. All Rights Reserved.<br/>Computer Science and Engineering Association,<br/> Department of Computer Science and Engineering,<br/> Kongu Engineering College</i></p>
-         <br/><br/><p>Developed and maintained by <a href='https://calibertech.netlify.app/' style={{color: 'white', textDecoration: 'none'}}><i>©Caliber Tech</i></a> , Sharvitha D</p>
+         <br/><br/><p>Developed and maintained by <a href='https://calibertech.netlify.app/' style={{color: 'white', textDecoration: 'none'}}>Karthickrajan S<i>(©Caliber Tech)</i></a> , Sharvitha D</p>
       </div>
     </div>
   )
