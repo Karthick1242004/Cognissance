d10b76b6c11fccdef1149647f21ec9f2ec1fb7b5
karthick <93571352+Karthick1242004@users.noreply.github.com>
2024-03-23 14:21:02 +0530

    Update Bitwizard.jsx

--- src/components/Eventlist/Bitwizard.jsx
+++ src/components/Eventlist/Bitwizard.jsx
@@ -7,7 +7,7 @@ const BitWizard= () => {
             <div className="max-w-4xl p-6">
             <h1 className={Eo.func}>BitWizard</h1>
                 <p className={Eo.func2}>Only for the mentioned departments: AUTO, CHEM, CIVIL, EEE, ECE, EIE, FT, MECH and MTS.Get ready for the coding challenge of a lifetime in Bit Realm! It's not just about code; it's a test of your problem-solving skills, algorithmic finesse, and coding prowess. This is your chance to shine.</p>
-                <div className={Eo.div}><button className={Eo.button}><a href="https://forms.gle/pbgjhGSUpmreFDh97" target="_blank" rel="noopener noreferrer" className={Eo.anti}>Register Now</a></button></div>
+                <div className={Eo.div}><button className={Eo.button}><a href="https://forms.gle/gqMbUr9ZFedBRdJu5" target="_blank" rel="noopener noreferrer" className={Eo.anti}>Register Now</a></button></div>
                 <h2 className={Eo.h2}>Guidelines</h2>
                 <ul className={Eo.ul}>
                   <li>Individual programming event </li>
